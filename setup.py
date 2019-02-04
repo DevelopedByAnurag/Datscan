e1b51ca68cb5ec9f330b7ccaab445df8eb354d38
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2019-02-05 01:10:29 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+   name='DatScAn',
+   version='1.0.0',
+   description="An automated data scraper and  Analyser tool",
+   long_description=long_description,
+   url='https://github.com/DevelopedByAnurag/Datscan/',
+   packages=['/'],
+   install_requires=requirements
+)

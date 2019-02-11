458dcf0b293194fa51c014e9a6cb0bc2e00d8791
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2019-02-12 00:08:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as f:
     requirements = [line.strip() for line in f.readlines()]
 
 setup(
-   name='DatScAn',
+   name='DatScan',
    version='1.0.0',
    description="An automated data scraper and  Analyser tool",
    long_description=long_description,
